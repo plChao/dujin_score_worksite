ba5e63cd032656428ad135a87eed51f50861bef8
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2023-08-18 20:46:53 +0800

    [fix duplicate row in actual_exam_situation] 1. typo in data_clean 2. step update

--- data_clean/set_article_id_ching.py
+++ data_clean/set_article_id_ching.py
@@ -10,7 +10,7 @@ df_clean_2 = df_clean.drop_duplicates(subset='姓名')
 df_clean_2 = df_clean_2[['姓名','A 基礎道學','B 啟蒙文學','C 古文詩詞選','D 三教經典','E 白陽經典','F 仙佛聖訓','G 仙佛聖訓','H 善歌','經典獎']]
 # 得到全部的段
 df_clean_2 = df_clean_2.fillna("")
-df_clean_2['seg'] = df_clean_2['A 基礎道學'] + ',' + df_clean_2['B 啟蒙文學'] + ','  + df_clean_2['C 古文詩詞選'] + ',' + df_clean_2['D 三教經典']+ ','  + df_clean_2['E 白陽經典']+ ','  + df_clean_2['F 仙佛聖訓']+ ','  + df_clean_2['G 仙佛聖訓']+ ','  + df_clean_2['G 仙佛聖訓']
+df_clean_2['seg'] = df_clean_2['A 基礎道學'] + ',' + df_clean_2['B 啟蒙文學'] + ','  + df_clean_2['C 古文詩詞選'] + ',' + df_clean_2['D 三教經典'] + ','  + df_clean_2['E 白陽經典']+ ','  + df_clean_2['F 仙佛聖訓']+ ','  + df_clean_2['G 仙佛聖訓']+ ','  + df_clean_2['H 善歌']
 # print(df_clean_2.iloc[1]['seg'])
 
 df = pd.DataFrame()
@@ -28,4 +28,5 @@ df['score_id']=""
 df_type = pd.read_csv('../2023_table/all_examinee_info.csv')[['name', 'exam_id']]
 print('df', len(df), 'df_examinee', len(df_type))
 df = df.merge(df_type, how='left')
+print(len(df), len(df.drop_duplicates(keep=False)))
 df.to_csv('../2023_table/actual_exam_situation.csv', index=False)
