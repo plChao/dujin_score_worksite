c092c78afc05f1dd86814e80ada7813f2b24f175
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2024-04-10 22:00:45 +0800

    [linking mydb sucessfully but djangosite did not wait the db finish]

--- code/score_site/settings.py
+++ code/score_site/settings.py
@@ -64,18 +64,18 @@ WSGI_APPLICATION = 'score_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': os.environ.get('DJANGO_MYSQL_DATABASE') or 'djangoblog',
-    #     'USER': os.environ.get('DJANGO_MYSQL_USER') or 'root',
-    #     'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD') or 'root',
-    #     'HOST': os.environ.get('DJANGO_MYSQL_HOST') or '127.0.0.1',
-    #     'PORT': int(os.environ.get('DJANGO_MYSQL_PORT') or 3306),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DJANGO_MYSQL_DATABASE') or 'djangoblog',
+        'USER': os.environ.get('DJANGO_MYSQL_USER') or 'root',
+        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD') or 'root',
+        'HOST': os.environ.get('DJANGO_MYSQL_HOST') or '127.0.0.1',
+        'PORT': int(os.environ.get('DJANGO_MYSQL_PORT') or 3306),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
